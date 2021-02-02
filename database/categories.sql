fcbb2cb2e8776798f16c9a51a02e29e9b70f9d48
Térence Meunier <terence.meunier@gmail.com>
2021-02-02 22:00:00 +0100

    database(dump): insert categories in the database dump file

---
+++ database/categories.sql
@@ -0,0 +1,15 @@
+-- Adminer 4.7.8 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1,	'Hardware'),
+(2,	'Software'),
+(3,	'Developement'),
+(4,	'Programmation'),
+(5,	'Systemes');
+
+-- 2021-02-02 20:57:28
