4a38c82a661764b1e6ee0b1a832f84e13c7365bc
Greg MAZA <gregory.maza@le-campus-numerique.fr>
2021-02-03 13:30:22 +0100

    fix : update route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
-require '../app/helpers/debug.php';
-require  '../config/database.php';
-require '../config/app.php';
-require '../routes/web.php';
+require 'app/helpers/debug.php';
+require  'config/database.php';
+require 'config/app.php';
+require 'routes/web.php';
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 $routes=[
-    '404' => '../ressources/views/errors/404.php',
-    'product' => '../app/controllers/productController.php'
-
+    '404' => 'ressources/views/errors/404.php',
+    'product' => 'app/controllers/productController.php',
+    'home' => 'app/controllers/homeController.php'
 ];
 
