f12c7f3f33161ffed10e0129102f06f31508c66a
Sarah Hans <sarah@Sarahui-MacBookAir.local>
2021-02-08 15:36:31 +0100

    Modification style page d'accueil

--- resources/views/home.php
+++ resources/views/home.php
@@ -6,10 +6,13 @@
                     <header>
                         <a href="/?action=product&id=<?= $product['id'] ?>"><h3><?= $product['title'] ?></h3></a>
                     </header>
-                    <a href="/?action=product&id=<?= $product['id'] ?>"><img
-                                src="resources/img/<?= $product['id'] ?>.jpg" alt="<?= $product['title'] ?>"
-                                height="150"/></a>
-                    <p><?= $product['description'] ?></p>
+                    <div class="product_content">
+                        <a href="/?action=product&id=<?= $product['id'] ?>"><img
+                                    src="resources/img/<?= $product['id'] ?>.jpg" alt="<?= $product['title'] ?>"
+                                    height="150"/></a>
+                        <p><?= $product['description'] ?></p>
+                    </div>
+
                     <footer>
                         <em>Prix HT : <?= number_format($product['price_ht'], 2, '.', ' ') ?> €/HT</em> | <em>Poids
                             : <?= $product['weight'] ?>
--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -6,37 +6,38 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="resources/css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Varela+Round&display=swap" rel="stylesheet">
     <title>BookDev</title>
 </head>
 <body>
-<header>
-    <h2> La boutique du Dev </h2>
+<header id="header_general">
+    <p id="logo"> La boutique du Dev </p>
+    <nav class="menu">
+        <div class="accueil"><a href="/">Accueil</a></div>
+            <?php if (isset($_SESSION['cart'])): ?>
+        <a href="?action=cart"><div class="cart">
+                    <header>
+                        <h3>Panier :</h3>
+                    </header>
+                    <?php if ($_SESSION['totalAndQuantities'][0] > 0): ?>
+                        <p><?= $_SESSION['totalAndQuantities'][1] ?> produits dans le panier</p>
+                        <footer>
+                            <em> Prix total du panier : <?= number_format($_SESSION['totalAndQuantities'][0],
+                                    2, '.', ' ') ?> €/TTC</em>
+                        </footer>
+                    <?php else: ?>
+                        <p>0 produits</p>
+                    <?php endif; ?>
+            </div></a>
+            <?php else: ?>
+        <a href="?action=cart"><div class="cart">
+                    <header>
+                        <h3>Panier</h3>
+                    </header>
+                    <p>0 produits</p>
+            </div></a>
+            <?php endif; ?>
+        </ul>
+    </nav>
 </header>
-<nav class="menu">
-    <ul>
-        <li><a href="/">Accueil</a></li>
-        <?php if (isset($_SESSION['cart'])): ?>
-            <li class="cart">
-                <header>
-                    <h3><a href="?action=cart">Panier</a></h3>
-                </header>
-                <?php if ($_SESSION['totalAndQuantities'][0] > 0): ?>
-                    <p><?= $_SESSION['totalAndQuantities'][1] ?> produits dans le panier</p>
-                    <footer>
-                        <em> Prix total du panier : <?= number_format($_SESSION['totalAndQuantities'][0],
-                                2, '.', ' ') ?> €/TTC</em>
-                    </footer>
-                <?php else: ?>
-                    <p>Panier vide : Prière d'acheter</p>
-                <?php endif; ?>
-            </li>
-        <?php else: ?>
-            <li class="cart">
-                <header>
-                    <h3><a href="?action=cart">Panier</a></h3>
-                </header>
-                <p>Panier vide : Prière d'acheter</p>
-            </li>
-        <?php endif; ?>
-    </ul>
-</nav>
